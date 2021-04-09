69e1939cefb78ec9a94a723a7f9127401c89ffe5
Fabian <lindgrenfabian@gmail.com>
2021-04-09 16:59:56 +0200

    remove character limit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ connection.connect(function (e) {
     }
 
     console.log(`\nConnected to MySQL (${process.env.mysql_database})\n`);
-    connection.query(`SELECT id,parsedName,largeImage,source FROM characters ORDER BY likeRank ASC LIMIT 20000`, function (err, result) {
+    connection.query(`SELECT id,parsedName,largeImage,source FROM characters ORDER BY likeRank ASC`, function (err, result) {
         if (err) throw err;
         else {
             result.forEach(char => {
