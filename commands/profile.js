192df65ed0e28c62c1045baf5c51a0c5d2a36b54
Fabian <lindgrenfabian@gmail.com>
2021-04-10 16:37:20 +0200

    add basic profile

---
+++ commands/profile.js
@@ -0,0 +1,45 @@
+var Discord = require("discord.js");
+var config = require("../config.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    let target = message.mentions.users.first() || message.author;
+
+    connection.query(`SELECT characters FROM users WHERE id = '${target.id}'`, function (err, result) {
+        if (err) throw err;
+        else {
+            var unique = 0;
+            var total = 0;
+            var header = "https://i.imgur.com/ILbATq4.jpg";
+            //user found
+            if (result.length > 0) {
+                var characters = JSON.parse(result[0].characters);
+                header = characterMap[characters[0].id.toString()].image;
+                characters.forEach(char => {
+                    unique++;
+                    total += char.amount;
+                })
+            }
+
+            var dupes = total == 0 ? 100 : Math.round((total - unique) / total * 100);
+
+            var characterEmbed = new Discord.MessageEmbed()
+                .setAuthor(target.username, target.displayAvatarURL({
+                    format: 'png',
+                    dynamic: true,
+                    size: 256
+                }))
+                .setThumbnail(header)
+                .setDescription(`
+                **Total characters**: ${total}
+                
+                **Unique characters**: ${unique} (${100-dupes}%)
+                **Duplicate characters**: ${total-unique} (${dupes}%)`)
+                
+            message.channel.send(characterEmbed)
+        }
+    })
+}
+module.exports.help = {
+    name: ["profile", "pr"]
+}
