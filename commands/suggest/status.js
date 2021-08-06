d840aa9e3450336905a5b5b64ae0f5eef6f6a71a
Fabian <lindgrenfabian@gmail.com>
2021-08-06 18:19:06 +0200

    add method to deny suggestion

--- commands/suggest/status.js
+++ commands/suggest/status.js
@@ -8,10 +8,12 @@ module.exports.run = async (bot, message, args) => {
         message.channel.messages.fetch(message.reference.messageId).then(msg => {
             var suggesterID = /(\d+)>/gm.exec(msg.content)[1];
             var suggester = bot.users.cache.get(suggesterID);
+
+            //APPROVE suggestion
             if (status == "approve") {
                 var embed = msg.embeds[0]
                     .setColor("GREEN")
-                    .addField(`:bookmark: ${message.author.username} approved this character!`, `${text}\n\n${Date.now()}`)
+                    .addField(`:white_check_mark: ${message.author.username} __approved__ this character!`, `${text}\n\n${Date.now()}`)
 
                 msg.edit({
                     embeds: [embed]
@@ -22,11 +24,29 @@ module.exports.run = async (bot, message, args) => {
                 suggester.send({
                     content: `Hey **${suggester.username}**, thank you for your character submission to SaltBot! Your suggestion has been __approved__ and will be added to the global character pool shortly :sparkles:
 \nA loot box have been added to your account that you can open with the **-loot** command. 
-\n${text.length > 1 ? `:envelope_with_arrow: **Approver Notes**: ${text}` : ""}`,
+\n${text.length > 1 ? `:envelope_with_arrow: **Approver Notes**: ${text}\n\u200B` : ""}`,
                     embeds: [cleanEmbed]
                 })
-            } else if (status == "deny") {
+                message.react("✅");
 
+                //DENY suggestion
+            } else if (status == "deny") {
+                var embed = msg.embeds[0]
+                    .setColor("RED")
+                    .addField(`:x: ${message.author.username} __denied__ this character!`, `${text}\n\n${Date.now()}`)
+
+                msg.edit({
+                    embeds: [embed]
+                });
+
+                var cleanEmbed = msg.embeds[0];
+                cleanEmbed.fields = [];
+                suggester.send({
+                    content: `Hey **${suggester.username}**, thank you for your character submission to SaltBot! Unfortunately your suggestion has been __denied__ and will not be added to the global character pool for now.
+\n${text.length > 1 ? `:envelope_with_arrow: **Approver Notes**: ${text}\n\u200B` : ""}`,
+                    embeds: [cleanEmbed]
+                })
+                message.react("✅");
             }
         });
 
