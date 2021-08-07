9eec9333230879f9f4d4a7ea04fd27beb7552004
Fabian <lindgrenfabian@gmail.com>
2021-08-07 21:46:33 +0200

    update help

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ var help = new Discord.MessageEmbed()
     .setColor("#fff8f7")
     .setTitle("Gacha")
     .addField(
-        "Commands 💵",
+        "Rolling 💵",
         `**-roll** Roll a random character to claim
 **-mm [user]** Check your claimed characters
 **-im <character>** Search for a character
@@ -18,6 +18,22 @@ var help = new Discord.MessageEmbed()
 **-profile [user]** Check your profile stats
 **-suggest** Suggest a character and get a reward
 \u200B`)
+    .addField(
+        "Wishlisting 🌟",
+        `**-wish <character(s)>** will give you higher odds of rolling them
+**-wishlist [user]** will show your wishlist
+**-wishremove <character(s)>** will remove a character from your wishlist
+**-wishd <character(s)>** will wishlist a character and then delete the message
+**-wishpurge** will remove all wishlisted characters that you already own
+\u200B`
+    )
+    .addField(
+        "Loot boxes 📦",
+        `**-loot** will open a loot box
+**-myloot [user]** will show your gained loot
+**-lootinfo** will show info about all available loots and how to get them
+\u200B`
+    )
     .addField(
         "Flags 🚩",
         `*Add these flags to commands to sort your claimed characters and show additional info about them, for example **-mma**.*
