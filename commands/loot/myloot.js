e2d75b15389f01c297dc40b8a80346eb7ce6d549
Fabian <lindgrenfabian@gmail.com>
2021-08-17 21:33:41 +0200

    use promises for lkl

--- commands/loot/myloot.js
+++ commands/loot/myloot.js
@@ -1,15 +1,8 @@
 module.exports.run = async (bot, message, args) => {
     let target = message.mentions.users.first() || message.author;
-    connection.query(`SELECT id,loots,lootboxes FROM users WHERE id = ${target.id}`, function (err, result) {
-        var myLoot = [];
-        var myLootCount = 0;
-        if (result.length > 0 && result[0].loots) {
-            myLoot = JSON.parse(result[0].loots);
-            myLootCount = result[0].lootboxes;
-        }
-
-        var desc = `Loot boxes saved: **${myLootCount}**\n\n`;
-        myLoot.forEach(loot => {
+    UserManager.getLoots(target.id).then(res => {
+        var desc = `Loot boxes saved: **${res.count}**\n\n`;
+        res.loot.forEach(loot => {
             desc += `${LootManager.loots[loot.id].emote} ${LootManager.loots[loot.id].plural}: **${loot.amount}**\n`
         })
 
