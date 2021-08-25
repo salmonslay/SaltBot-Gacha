5a907f9a8e9ffcd6a00e787a1009844d61eae74f
Fabian <lindgrenfabian@gmail.com>
2021-08-25 16:22:40 +0200

    require a query to do -im

--- commands/im.js
+++ commands/im.js
@@ -1,11 +1,15 @@
 module.exports.run = async (bot, message, args) => {
-    //query user searched for
-    var query = args.join(' ');
-    var result = utils.findCharacter(query);
+    if (args.length > 0) {
+        //query user searched for
+        var query = args.join(' ');
+        var result = utils.findCharacter(query);
 
-    if (result.best) sendChar(message, result.best);
-    else if (result.matches.length == 0) message.channel.send(`${message.author.toString()}, no character found.`)
-    else message.channel.send(result.text);
+        if (result.best) sendChar(message, result.best);
+        else if (result.matches.length == 0) message.channel.send(`${message.author.toString()}, no character found.`)
+        else message.channel.send(result.text);
+    } else {
+        message.channel.send("Syntax: **-im <character>**\n\n*Searches for a character in the data base*");
+    }
 }
 
 function sendChar(message, character) {
